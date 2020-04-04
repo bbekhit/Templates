ca9769b575336a644d70648c69cae307dc017e52
Boutros Bekhit <bbekhit@apple.com>
2020-04-04 13:23:29 -0700

    addition-1

---
+++ client/src/components/Test/Test.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      <div className="parent">
+        <div className="child-1">child-1</div>
+        <div className="child-2">child-2</div>
+        <div className="child-3">child-3</div>
+      </div>
+      <div className="test">Hello there form test element</div>
+    </div>
+  );
+};
+
+export default Test;
